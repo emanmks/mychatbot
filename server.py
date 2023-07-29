2e2f5c595ae8332bd94aa61b5e0272e29fa511aa
Solaiman Mansyur <solaiman.mansyur@gmail.com>
2023-07-29 17:39:42 +0800

    introduce svelte

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return "Hello, World!"
+    return send_from_directory('client/dist/', 'index.html')
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route('/<path:path>')
+def static_proxy(path):
+    return send_from_directory('client/dist/', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
